2cb7dc68b926715ba7305e437aeb4fa9cfc67503
SalteWalte <61295061+WalterKeiler@users.noreply.github.com>
2022-04-08 16:47:44 -0400

    Grapple type thing implemented

--- Assets/PlayerController.cs
+++ Assets/PlayerController.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
+    private List<Vector3> clickedPos;
+
+    private Rigidbody2D rb;
+    private Vector2 targetPos;
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
     
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            Vector3 mouse = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
+            clickedPos.Add(mouse);
+            float angle = Mathf.Atan2(mouse.y, mouse.x) * Mathf.Rad2Deg;
+            gameObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            rb.AddForce(transform.right * speed);
+        }
     }
 }
