81cf1d536229953f8c8f7b2693d1b61d8c056122
Walter Keiler <walterdavidkeiler@gmeil.com>
2022-04-11 10:32:20 -0400

    work on implementing line of best fit

--- Assets/_Scripts/PlayerController.cs
+++ Assets/_Scripts/PlayerController.cs
@@ -1,11 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float speed;
-    private List<Vector2> clickedPos;
+    [SerializeField] private List<Vector2> clickedPos;
+    [SerializeField] private double w;
+    [SerializeField] private double b;
+    [SerializeField] private float learningRate;
+    [SerializeField] private float normalize;
+    [SerializeField] private LineRenderer line;
+    [SerializeField] private Transform parentTransform;
 
     private Rigidbody2D rb;
     private Vector2 targetPos;
@@ -23,10 +30,52 @@ public class PlayerController : MonoBehaviour
             clickedPos.Add(targetPos);
             float angle = Mathf.Atan2(targetPos.y, targetPos.x) * Mathf.Rad2Deg;
             gameObject.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+            GradientDescent();
+            SetLine(w,b);
         }
         if (Input.GetKey(KeyCode.Mouse0))
         {
             rb.AddForce(transform.right * speed);
         }
+    }
+    
+    private void SetLine(double w, double b)
+    {
+        // double rad = Math.Atan(w);
+        // double deg = rad * 180 / Math.PI;
+        // transform.localPosition = new Vector3(0, (float) b, 0);
+        // transform.localRotation = Quaternion.Euler(0, 0, (float) deg);
+
+        line.SetPosition(0, new Vector3(-2.5f + parentTransform.localPosition.x, (-0.5f* (float) w + (float) b) * 5f + parentTransform.localPosition.y, 0f));
+        line.SetPosition(1, new Vector3(2.5f + parentTransform.localPosition.x, (0.5f* (float) w + (float) b) * 5f + parentTransform.localPosition.y, 0f));
+
+
+    }
+    
+    private void GradientDescent()
+    {
+        var pointList = clickedPos.ToArray();
+        var positions = pointList.Select(p => p);
+        var X = positions.Select(pos => pos.x);
+        // foreach (var x in X)
+        // {
+        //     Debug.Log(x);
+        // }
+        var Y = positions.Select(pos => pos.y);
+        int n = pointList.Length;
+        var Y_pred = X.Select(x => x * w + b);
+
+        double dW = -2.0 / n * Y
+            .Zip(Y_pred, (y, yPred) => y - yPred)
+            .Zip(X, (y_p, x) => y_p * x)
+            .Sum();
+        double dB = -2.0 / n * Y
+            .Zip(Y_pred, (y, yPred) => y - yPred)
+            .Sum();
+
+        // Debug.Log(dW + " : " + dB);
+
+        w -= learningRate * dW * normalize;
+        b -= learningRate * dB * normalize;
     }
 }
