591331a464025866efc5197eaa532d3858e6d140
Walter Keiler <walterdavidkeiler@gmeil.com>
2022-04-20 16:08:12 -0400

    Update Hexagon Flat-Top.prefab and HexGridManager.cs

--- Assets/_Scripts/HexGridManager.cs
+++ Assets/_Scripts/HexGridManager.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HexGridManager : MonoBehaviour
 {
@@ -11,12 +12,15 @@ public class HexGridManager : MonoBehaviour
     [SerializeField] private GameObject hexPrefab;
     [SerializeField] private float offset;
 
-    private Vector3[] hexesPos;
+    private Hex[] hexes;
+    private Vector3 currentPos;
     
     private void Start()
     {
         int[,] grid = new int[gridHeight, gridWidth];
         float[,] value = new float[gridWidth, gridHeight];
+
+        hexes = new Hex[gridHeight + gridWidth];
         
         //for (int x = 0; x < gridWidth; x++)
         //{
@@ -30,13 +34,16 @@ public class HexGridManager : MonoBehaviour
 
     public void CreateHexGrid(int[,] grid, float[,] value, float hexSize)
     {
-        Debug.Log("x: " + value.GetLength(1) + " y: " + value.GetLength(0));
 
         for (int x = 0; x < grid.GetLength(1); x++)
         {
             for (int y = 0; y < grid.GetLength(0); y++)
             {
                 float val = value[x,y];
+                Hex test = new Hex(new Vector3(x, y, -x-y), 0);
+                hexes[x+y] = test;
+                currentPos = test.position;
+                Debug.Log(test.position);
                 CreateHex(new Vector2(x, y), 0, hexSize);
             }
         }
@@ -55,6 +62,7 @@ public class HexGridManager : MonoBehaviour
         GameObject hexObj = Instantiate(hexPrefab, Vector3.zero, Quaternion.identity, transform);
         hexObj.transform.localScale = Vector3.one * hexSize;
         hexObj.transform.localPosition = pos;
+        hexObj.GetComponentInChildren<Text>().text = currentPos.ToString();
         //hexes[(int) pos.x, (int) pos.y] = hexObj;
     }
 }
@@ -67,7 +75,7 @@ public class HexGrid
         {
             for (int y = 0; y < grid.GetLength(0); y++)
             {
-                new Hex(new Vector2(x, y), value[x, y], hexSize);
+                new Hex(new Vector2(x, y), value[x, y]);
             }
         }
     }
@@ -75,8 +83,11 @@ public class HexGrid
 
 public class Hex
 {
-    public Hex(Vector2 pos, float value, float size)
+    public Vector3 position;
+    public float hexValue;
+    public Hex(Vector3 pos, float value)
     {
-        
+        position = pos;
+        hexValue = value;
     }
 }
