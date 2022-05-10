f0bace08d48fff55e6999be272372fde09a6d73d
StealthTom <seafordtbf@gmail.com>
2022-05-09 23:17:03 -0400

    Bit more controller work.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
 
     public CharacterController Controller;
     public Vector3 Velocity;
+    public GameObject Grid;
 
     // Start is called before the first frame update
     void Start()
@@ -18,25 +19,25 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow)) {
-
+            // forward
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow)) {
-
+            // left
         }
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow)) {
-
+            // right
         }
         if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow)) {
-
+            // back
         }
         if (Input.GetKey(KeyCode.Space)) {
-
+            // jump
         }
         if (Input.GetKey(KeyCode.R)) {
-
+            Grid.SetActive(!Grid.activeInHierarchy);
         }
         if (Input.GetKey(KeyCode.F)) {
-
+            // check for a node
         }
     }
 }
