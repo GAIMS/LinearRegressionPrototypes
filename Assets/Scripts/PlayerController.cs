1478895b72b20b042c375e1ec8e1bd77e27ecade
StealthTom <seafordtbf@gmail.com>
2022-05-16 23:39:06 -0400

    More basic controls added.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,11 @@ public class PlayerController : MonoBehaviour
 
     public CharacterController Controller;
     public Vector3 Velocity;
+    public GameObject Tablet;
     public GameObject Grid;
+    public GameObject Map;
+    public GameObject Marker;
+    public Transform MarkerParent;
     public float Speed;
     public float Jump;
     public float Gravity = -9.81f;
@@ -38,6 +42,24 @@ public class PlayerController : MonoBehaviour
         if (Input.GetButtonDown("Jump") && groundedPlayer)
         {
             Velocity.y += Mathf.Sqrt(Jump * Gravity);
+        }
+
+        if (Input.GetKeyDown(KeyCode.F)) {
+            bool set = !Map.activeInHierarchy;
+            Tablet.SetActive(set);
+            Grid.SetActive(set);
+            Map.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.E)) {
+            bool set = !Map.activeInHierarchy;
+            Tablet.SetActive(set);
+            Map.SetActive(set);
+            Grid.SetActive(false);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Instantiate(Marker, new Vector3(this.transform.position.x, 500f, this.transform.position.z), new Quaternion());
         }
 
         Velocity.y += Gravity * Time.deltaTime;
