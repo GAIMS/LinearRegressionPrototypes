07dfd51d147f79a2317be442ca8caccc304d92e0
Walter Keiler <walterdavidkeiler@gmeil.com>
2022-05-25 10:20:11 -0400

    Update Assembly-CSharp.csproj, Bot-Red.prefab, and 6 more files...

--- Assets/_Scripts/CameraController.cs
+++ Assets/_Scripts/CameraController.cs
@@ -1,26 +1,36 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Cinemachine;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
     private List<BotController> bots;
     private RaceTrackGenerator rtg;
+    private CinemachineTargetGroup targetGroup;
+    [SerializeField] private float radius = 25f;
     void Start()
     {
         rtg = FindObjectOfType<RaceTrackGenerator>();
         //bots = FindObjectsOfType<BotController>();
+        targetGroup = GetComponentInChildren<CinemachineTargetGroup>();
         FindBots();
     }
 
     public void FindBots()
     {
         bots = new List<BotController>();
+        targetGroup.m_Targets = new CinemachineTargetGroup.Target[rtg.racers];
         for (int i = 0; i < rtg.racers; i++)
         {
             bots.Add(FindObjectsOfType<BotController>()[i]);
+            targetGroup.m_Targets[i].target = bots[i].transform;
+            targetGroup.m_Targets[i].weight = 1;
+            targetGroup.m_Targets[i].radius = radius;
         }
+
+        
     }
     
     void Update()
@@ -37,6 +47,17 @@ public class CameraController : MonoBehaviour
         }
         var centerX = totalX / bots.Count;
         var centerY = totalY / bots.Count;
-        transform.position = new Vector3(centerX, centerY, transform.position.z);
+        //transform.position = new Vector3(centerX, centerY, transform.position.z);
+    }
+
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        GetComponent<CinemachineVirtualCamera>().m_Lens.OrthographicSize = 35;
+    }
+
+    public void UnPause()
+    {
+        Time.timeScale = 1;
     }
 }
